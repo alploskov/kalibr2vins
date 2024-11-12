537c62e91fb99d3a072163d33b6b0682fb1b0983
nbaldy-hb <155659719+nbaldy-hb@users.noreply.github.com>
2024-11-12 12:52:57 -0500

    Add pinhole-equidistant translation
    Relevant files and related documentation: 
    - https://github.com/zinuok/VINS-Fusion-ROS2/blob/main/camera_models/src/camera_models/EquidistantCamera.cc
    - https://github.com/ethz-asl/kalibr/wiki/supported-models#references
    - https://docs.opencv.org/3.4/db/d58/group__calib3d__fisheye.html#details

--- kalibr2vins/cameras_templates.py
+++ kalibr2vins/cameras_templates.py
@@ -37,8 +37,27 @@ projection_parameters:
    u0: {intrinsics[3]}
    v0: {intrinsics[4]}
 """
+KANNALA_BRANDT = """%YAML:1.0
+---
+model_type: KANNALA_BRANDT
+camera_name: {name}
+image_width: {resolution[0]}
+image_height: {resolution[1]}
 
+distortion_parameters:
+   # Note that EquidistantCamera uses 'k_N+1' to describe 'k_N' in the Kalibr wiki's equidistant implementation
+   k2: {distortion_coeffs[0]}
+   k3: {distortion_coeffs[1]}
+   k4: {distortion_coeffs[2]}
+   k5: {distortion_coeffs[3]}
+projection_parameters:
+   mu: {intrinsics[0]}
+   mv: {intrinsics[1]}
+   u0: {intrinsics[2]}
+   v0: {intrinsics[3]}
+"""
 TEMPLATES = {
     'pinhole-radtan': PINHOLE_RADTAN,
     'omni-radtan': OMNI_RADTAN,
+    'pinhole-equidistant': KANNALA_BRANDT,
 }
